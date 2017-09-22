04e2f6b48894020873e1460b3b087de218d2cdc2
heyongyi <hyy92010991@126.com>
2017-09-22 15:42:32 +0800

    双数据源支持 补

---
+++ src/main/java/org/sumbootFrame/data/dao/secondary/defaulttest/SecondaryDAO.java
@@ -0,0 +1,10 @@
+package org.sumbootFrame.data.dao.secondary.defaulttest;
+
+import org.sumbootFrame.mvc.interfaces.IDao;
+
+import java.util.List;
+
+public interface SecondaryDAO extends IDao {
+
+	public List<?> getEcsAreainfo();
+}
